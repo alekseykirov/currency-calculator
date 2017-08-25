18aabe7891c8d7baf050f74d970a26cc526f5893
Aleksey Kirov <aleksey.kirov@yandex.ru>
2017-08-25 14:50:43 +0700

    * js style code

--- js/modules/main.js
+++ js/modules/main.js
@@ -3,66 +3,66 @@ jQuery(document).ready(function () {
     function getCalkItemTeplate() {
         return '<div class="calc__item">' +
             '<label>' +
-            '<span class="calc__value"></span>' +
-            '<input type="text" class="calc__input active">' +
-            '<button class="button js-button_save">Сохранить</button>' +
+                '<span class="calc__value"></span>' +
+                '<input type="number" class="calc__input active">' +
+                '<button class="button button_save js-button_save">Сохранить</button>' +
             '</label>' +
-            '<a href="#" class="js-calc__delete">Удалить</a>' +
+                '<a href="#" class="delete js-calc__delete">Удалить</a>' +
             '</div>';
     }
 
+// add item
+
     var buttonAdd = $('.js-button_add');
     buttonAdd.on('click', function() {
-
         var check = jQuery('.active');
 
         if(check.val()) {
             check.siblings('.calc__value').append(check.val());
-
-            var twt =  jQuery('.calc__amount').text();
-            var sum = +twt + +(check.val());
-            
-            jQuery('.calc__amount').empty().append(sum);
+            var total = jQuery('.js-calc__amount');
+            var totalValue =  total.text();
+            var result = +totalValue + +(check.val());
+            total.empty().append(result);
         } else {
             check.siblings('.calc__value').append(0)
         }
 
         check.hide();
         check.siblings('.button').hide();
-        
-        jQuery('.calc__input.active').removeClass('active');
 
+        check.removeClass('active');
         jQuery('.calc__list').append(getCalkItemTeplate())
-        
     });
 
 //save value
 
     jQuery('.calc__list').on('click','.js-button_save', function () {
-        var valIntup = jQuery(this).siblings('.calc__input').val();
+        var inputValue = jQuery(this).siblings('.calc__input').val();
 
-        jQuery(this).siblings('.calc__value').append(valIntup);
+        jQuery(this).siblings('.calc__value').append(inputValue);
         jQuery(this).siblings('.calc__input').removeClass('active').hide();
         jQuery(this).hide();
 
-        var twt =  jQuery('.calc__amount').text();
-        var sum = +twt + +valIntup;
+        var total = jQuery('.js-calc__amount');
+        var totalValue =  total.text();
+        var result = +totalValue + +inputValue;
         
-        jQuery('.calc__amount').empty().append(sum);
+        total.empty().append(result);
     });
 
 //edit value
 
     jQuery('.calc__list').on('click','.calc__value', function () {
-        var beforeVal = jQuery(this).text();
+        var beforeValue = jQuery(this).text();
         jQuery(this).empty();
         jQuery(this).siblings('.calc__input').addClass('active').show();
         jQuery(this).siblings('.js-button_save').show();
-        
-        var titgi = jQuery('.calc__amount').text();
-        var totp = +titgi - +beforeVal;
 
-        jQuery('.calc__amount').empty().append(totp);
+        var total = jQuery('.js-calc__amount');
+        var totalValue =  total.text();
+        var result = +totalValue + +beforeValue;
+
+        jQuery('.calc__amount').empty().append(result);
 
     });
     
@@ -73,9 +73,10 @@ jQuery(document).ready(function () {
         parentItem.remove();
 
         var delVal = parentItem.find('.calc__value').text();
-        var titSum = jQuery('.calc__amount').text();
-        var toot = +titSum - +delVal;
+        var total = jQuery('.js-calc__amount');
+        var totalValue =  total.text();
+        var result = +totalValue + +delVal;
         
-        jQuery('.calc__amount').empty().append(toot);
+        jQuery('.calc__amount').empty().append(result);
     })
 });
